ba44abdbb7b6d347ffeda751a01684728e4f8a4c
tianzhiqi <qintianzhiqi@gmail.com>
2017-04-12 23:36:53 +0800

    desc

--- src/product/ProductDetail.vue
+++ src/product/ProductDetail.vue
@@ -15,8 +15,19 @@
         </div>
     </nav-header>
     <!-- detail -->
-    <div class="weui-article">
-      <h3 class="product-name">{{exhibdetail.name}}</h3>
+    <div class="exhib-img">
+      <img :src="exhibdetail.titleImage" alt="">
+    </div>
+    <div class="weui-article exhib-detail">
+      <h3 class="product-name font-theme">{{exhibdetail.name}}</h3>
+      <div class="more-desc" v-html="exhibdetail.description">
+      </div>
+      <div class="desc-link">
+        <p class="text-right">
+          <a class="font-theme" :href="exhibdetail.productMoreDetail">{{$t("product.more")}} <i class="right-arrow"></i> </a>
+
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -40,4 +51,31 @@ export default {
 </script>
 
 <style lang="scss">
+.exhib-detail {
+  padding: 10px;
+  font-size: 14px;
+  background: #fff;
+  margin-top: -8px;
+  box-sizing: border-box;
+}
+.desc-link {
+  a {
+    position: relative;
+    padding-right: 13px;
+    &:after {
+      content: "";
+      display: inline-block;
+      height: 6px;
+      width: 6px;
+      border-width: 2px 2px 0 0;
+      border-color: #c8c8cd;
+      border-style: solid;
+      transform: matrix(.71,.71,-.71,.71,0,0);
+      position: absolute;
+      top: 50%;
+      margin-top: -4px;
+      right: 2px;
+    }
+  }
+}
 </style>
