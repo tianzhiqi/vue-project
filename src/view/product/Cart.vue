547c4979a3f46c46e433e24082e7245afaa2ab04
tianzhiqi <qintianzhiqi@hotmail.com>
2017-05-17 23:43:58 +0800

    todo: cart and shop order

---
+++ src/view/product/Cart.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="container">
+  <nav-header>
+      <div class="weui-flex" slot="nav">
+          <div class="weui-flex__item">
+              <a class="text-center font-theme">
+                <label>{{$t("product.cart")}}</label>
+              </a>
+          </div>
+      </div>
+      <div class="head-nav-icon" slot="icon">
+          <router-link :to="{name: 'profile'}">
+              <i class="user-icon"></i>
+          </router-link>
+      </div>
+  </nav-header>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
