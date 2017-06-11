52dacfcf2942614587ae21c2d0811a1a089a120a
tianzhiqi <qintianzhiqi@hotmail.com>
2017-06-11 21:48:05 +0800

    detail

--- src/view/order/OrderDetail.vue
+++ src/view/order/OrderDetail.vue
@@ -14,6 +14,22 @@
         </router-link>
       </div>
     </nav-header>
+    <div class="common-wrap">
+      <div class="order-detail">
+        <div class="shop-order_pro">
+          <div class="weui-cell" v-if="orderDetail.product">
+            <div class="img-wrap">
+              <img :src="orderDetail.product.coverImage" alt="">
+            </div>
+            <div class="weui-cell__bd">
+              <p class="text-title text-list">{{orderDetail.product.name}}</p>
+              <p class="text-list text-child">{{orderDetail.createdtime | date('YYYY-MM-DD HH:mm')}}</p>
+              <p class="shop-order_price"><span class="font-theme">{{orderDetail.product.price | price}}</span><span>&times;</span>{{orderDetail.quantity}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -37,5 +53,10 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+.order-detail {
+  .shop-order_pro {
+    background: #fff;
+  }
+}
 </style>
